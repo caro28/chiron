d632ca3a3d445e7824885cdbd79c32ea8ca2afc7
Caroline <caroline.c.craig@gmail.com>
2023-06-22 14:37:49 -0400

    lucretius preprocessing edits

--- align_texts_project/code/book-stream_lucretius_en1893.py
+++ align_texts_project/code/book-stream_lucretius_en1893.py
@@ -32,23 +32,23 @@ class BookStream(object):
                 cite = ','.join([x for x in self.divs if x != ''])
                 if cite != '':
                     ##### comment out rest of function to print without section markers #####
-                    print('#', cite, '\n')
+                    print('#@$%', cite, '#@$%\n')
             case 'note':
-                self.buf += '\n# note\n'
+                self.buf += '\n#@$%note#@$%\n'
             case 'title':
-                self.buf += '# title\n'
+                self.buf += '#@$%title#@$%\n'
     def end(self, elem):
         match etree.QName(elem).localname:
             case 'div':
                 self.divs.pop()
             case 'head' | 'p':
-                print(self.buf.strip(), '\n# paragraph end\n')
+                print(self.buf.strip(), '\n#@$%paragraph end#@$%\n')
                 ##### use line below instead to print without section markers #####
                 # print(self.buf.strip(), '\n')
                 self.buf = ''
             ##### comment out case "note" section below to print without section markers #####
             case 'note':
-                self.buf += '\n# note_end\n'
+                self.buf += '\n#@$%note_end#@$%\n'
             case 'title':
                 self.buf += '\n'
             #### add page numbers? code below doesn't work #####
